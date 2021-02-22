2052280336eab29a66ca2cea4d2f9a93e6a051c9
Alex <1070439064@qq.com>
2021-02-22 22:23:38 +0800

    添加的一个文件中添加了三个注释

---
+++ src/views/ab.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>This is an ab</h1>
+    <h1>{{sss}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Arya",
+  data() {
+    return {
+      name: "Arya",
+    };
+  },
+  computed:{
+    sss(){
+      return this.$route.params.ex
+    }
+  },
+  // 添加组件内守卫
+  // 又添加了一个注释
+  // 第三个注释
+  beforeRouteEnter: (to, from, next) => {
+    next((vm) => {
+      alert("hello" + vm.name);
+    });
+  },
+};
+</script>
+
